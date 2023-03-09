cb8fa680fe4b9bbc28790d933d2af6d5e96c755a
pedropuertasr <pedropuertasr@gmail.com>
2023-03-09 13:31:19 +0100

    Ejercicio terminado

---
+++ Agenda/app/src/main/java/com/pedropuertas/agenda/NuevoActivity.java
@@ -0,0 +1,59 @@
+package com.pedropuertas.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.pedropuertas.agenda.db.DbContactos;
+
+import java.text.SimpleDateFormat;
+
+public class NuevoActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtTelefono, txtCorreoElectronico, txtFecha;
+    Button btnGuarda;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreoElectronico = findViewById(R.id.txtCorreoElectronico);
+        txtFecha = findViewById(R.id.txtFecha);
+        btnGuarda = findViewById(R.id.btnGuarda);
+
+        btnGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!txtNombre.getText().toString().equals("") && !txtTelefono.getText().toString().equals("")) {
+
+                    DbContactos dbContactos = new DbContactos(NuevoActivity.this);
+                    long id = dbContactos.insertarContacto(txtNombre.getText().toString(), txtTelefono.getText().toString(), txtCorreoElectronico.getText().toString(), txtFecha.getText().toString());
+
+                    if (id > 0) {
+                        Toast.makeText(NuevoActivity.this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show();
+                        limpiar();
+                    } else {
+                        Toast.makeText(NuevoActivity.this, "ERROR AL GUARDAR REGISTRO", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(NuevoActivity.this, "DEBE LLENAR LOS CAMPOS OBLIGATORIOS", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void limpiar() {
+        txtNombre.setText("");
+        txtTelefono.setText("");
+        txtCorreoElectronico.setText("");
+        txtFecha.setText("");
+    }
+}
